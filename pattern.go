1bbaccf90aed5d993b6e6e685319d100aa275c84
JImmyFrasche <soapboxcicero@gmail.com>
2014-03-26 16:38:06 -0700

    moved pattern example from test to docs

--- pattern.go
+++ pattern.go
@@ -462,6 +462,41 @@ func (r RadialGradient) RadialCircles() (start, end Circle) {
 //
 //Additional patches may be added with additional calls to BeginPatch/EndPatch.
 //
+//	red, green, blue := Color{R: 1}, Color{G: 1}, Color{B: 1}
+//
+//	m := NewMesh()
+//
+//	//Add a Coons patch
+//	err := m.BeginPatch().
+//		MoveTo(ZP).
+//		CurveTo(Pt(30, -30), Pt(60, 30), Pt(100, 0)).
+//		CurveTo(Pt(60, 30), Pt(130, 60), Pt(100, 100)).
+//		CurveTo(Pt(60, 70), Pt(30, 130), Pt(0, 100)).
+//		CurveTo(Pt(30, 70), Pt(-30, 30), ZP).
+//		SetCornerColor(0, red).
+//		SetCornerColor(1, green).
+//		SetCornerColor(2, blue).
+//		SetCornerColor(3, Color{R: 1, G: 1}).
+//		EndPatch()
+//
+//	if err != nil {
+//		panic(err) //this is not how to handle errors, outside of examples
+//	}
+//
+//	//Add a Gouraud-shaded triangle
+//	err = m.BeginPatch().
+//		MoveTo(Pt(100, 100)).
+//		LineTo(Pt(130, 130)).
+//		LineTo(Pt(130, 70)).
+//		SetCornerColor(0, red).
+//		SetCornerColor(1, green).
+//		SetCornerColor(2, blue).
+//		EndPatch()
+//
+//	if err != nil {
+//		panic(err)
+//	}
+//
 //When two patches overlap, the last one that has been added is drawn over
 //the first one.
 //
