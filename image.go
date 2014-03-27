2f64e3fe9ca6014479ff791ed1c1e4e7f5550733
JImmyFrasche <soapboxcicero@gmail.com>
2014-03-26 19:03:32 -0700

    added read image from file

--- image.go
+++ image.go
@@ -1,8 +1,13 @@
 package cairo
 
 //#cgo pkg-config: cairo
+//#include <stdlib.h>
 //#include <cairo/cairo.h>
 import "C"
+
+import (
+	"unsafe"
+)
 
 type ImageSurface struct {
 	ExtensionSurface
@@ -10,25 +15,7 @@ type ImageSurface struct {
 	width, height, stride int
 }
 
-//Originally cairo_image_surface_create.
-func NewImageSurface(format format, width, height int) (ImageSurface, error) {
-	is := C.cairo_image_surface_create(format.c(), C.int(width), C.int(height))
-	stride := int(C.cairo_image_surface_get_stride(is))
-	s := ImageSurface{
-		ExtensionSurface: ExtensionNewSurface(is),
-		format:           format,
-		width:            width,
-		height:           height,
-		stride:           stride,
-	}
-	return s, s.Err()
-}
-
-func cNewImageSurface(s *C.cairo_surface_t) (Surface, error) {
-	format := format(C.cairo_image_surface_get_format(s))
-	width := int(C.cairo_image_surface_get_width(s))
-	height := int(C.cairo_image_surface_get_height(s))
-	stride := int(C.cairo_image_surface_get_stride(s))
+func newImg(s *C.cairo_surface_t, format format, width, height, stride int) (ImageSurface, error) {
 	S := ImageSurface{
 		ExtensionSurface: ExtensionNewSurface(s),
 		format:           format,
@@ -36,13 +23,40 @@ func cNewImageSurface(s *C.cairo_surface_t) (Surface, error) {
 		height:           height,
 		stride:           stride,
 	}
-
 	return S, S.Err()
 }
 
-//BUG(jmf): need safe wrapper around get_data
+//Originally cairo_image_surface_create.
+func NewImageSurface(format format, width, height int) (ImageSurface, error) {
+	is := C.cairo_image_surface_create(format.c(), C.int(width), C.int(height))
+	stride := int(C.cairo_image_surface_get_stride(is))
+	return newImg(is, format, width, height, stride)
+}
 
-//BUG(jmf): need image_surface_create_for_data analog(s)
+func cNewImageSurface(s *C.cairo_surface_t) (Surface, error) {
+	format := format(C.cairo_image_surface_get_format(s))
+	width := int(C.cairo_image_surface_get_width(s))
+	height := int(C.cairo_image_surface_get_height(s))
+	stride := int(C.cairo_image_surface_get_stride(s))
+
+	return newImg(s, format, width, height, stride)
+}
+
+//NewImageSurfaceFromPNGFile creates a new image surface and initializes
+//it with the contents of the given PNG file.
+//
+//Originally cairo_image_surface_create_from_png.
+func NewImageSurfaceFromPNGFile(filename string) (ImageSurface, error) {
+	f := C.CString(filename)
+	is := C.cairo_image_surface_create_from_png(f)
+	C.free(unsafe.Pointer(f))
+	s, err := cNewImageSurface(is)
+	return s.(ImageSurface), err
+}
+
+//BUG(jmf): ImageSurface: need safe wrapper around get_data
+
+//BUG(jmf): ImageSurface: need image_surface_create_for_data analog(s)
 
 //Format reports the format of the surface.
 //
