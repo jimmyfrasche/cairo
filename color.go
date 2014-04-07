d62f3d8175f73d8d6a7fb7763623c54a8802557b
JImmyFrasche <soapboxcicero@gmail.com>
2014-04-06 18:27:04 -0700

    fixed typo causing blue channel to be ignored on Color; added helpers and some basic color variables

--- color.go
+++ color.go
@@ -45,7 +45,12 @@ func (c Color) RGBA() (r, g, b, a uint32) {
 	return ctoi(c.R), ctoi(c.G), ctoi(c.B), 0xffff
 }
 
-//AlphaColor represents an RGBA color where each component is in [0, 1].
+//Alpha returns c with an alpha channel set to alpha.
+func (c Color) Alpha(alpha float64) AlphaColor {
+	return AlphaColor{c.R, c.G, c.B, alpha}
+}
+
+//AlphaColor represents an RGBA color where each component is in [0,1].
 type AlphaColor struct {
 	R, G, B, A float64
 }
@@ -69,11 +74,16 @@ func (a AlphaColor) Canon() AlphaColor {
 }
 
 func (a AlphaColor) c() (r, g, b, alpha C.double) {
-	return C.double(a.R), C.double(a.G), C.double(a.G), C.double(a.A)
+	return C.double(a.R), C.double(a.G), C.double(a.B), C.double(a.A)
 }
 
 func (a AlphaColor) RGBA() (r, g, b, alpha uint32) {
 	return ctoi(a.R), ctoi(a.G), ctoi(a.B), ctoi(a.A)
+}
+
+//Color returns a without the alpha channel.
+func (a AlphaColor) Color() Color {
+	return Color{a.R, a.G, a.B}
 }
 
 //These models can convert any color.Color to themselves.
@@ -84,6 +94,9 @@ var (
 		if c, ok := c.(Color); ok {
 			return c
 		}
+		if c, ok := c.(AlphaColor); ok {
+			return Color{c.R, c.G, c.B}
+		}
 		r, g, b, _ := c.RGBA()
 		return Color{cto01(r), cto01(g), cto01(b)}
 	})
@@ -91,7 +104,20 @@ var (
 		if c, ok := c.(AlphaColor); ok {
 			return c
 		}
+		if c, ok := c.(Color); ok {
+			return AlphaColor{c.R, c.G, c.B, 1}
+		}
 		r, g, b, a := c.RGBA()
 		return AlphaColor{cto01(r), cto01(g), cto01(b), cto01(a)}
 	})
+)
+
+//Basic colors.
+var (
+	Black       = Color{}
+	White       = Color{1, 1, 1}
+	Red         = Color{R: 1}
+	Green       = Color{G: 1}
+	Blue        = Color{B: 1}
+	Transparent = AlphaColor{}
 )
