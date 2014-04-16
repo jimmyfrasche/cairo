03c78431c6bf4ee3692bf512f71a3d09027a3432
JImmyFrasche <soapboxcicero@gmail.com>
2014-04-16 15:09:00 -0700

    made test more idiomatic

--- cairo_test.go
+++ cairo_test.go
@@ -16,10 +16,11 @@ func ExampleContext_Circle_drawEllipse() {
 	//This draws an ellipse in the box given by Rectangle r.
 	r := Rect(0, 0, 50, 60)
 	c.SaveRestore(func(c *Context) error {
-		mid := Pt(r.Dx()/2, r.Dy()/2)
-		c.Translate(r.Min.Add(mid))
-		c.Scale(mid)
-		c.Circle(UC)
+		mid := r.Size().Div(2)
+		c.
+			Translate(r.Min.Add(mid)).
+			Scale(mid).
+			Circle(UC)
 		return nil
 	})
 }
