a90ee3a5706d2608840ca91ca0c05e3ce19ed602
JImmyFrasche <soapboxcicero@gmail.com>
2014-05-08 23:07:33 -0700

    added hello world example to package

--- cairo_test.go
+++ cairo_test.go
@@ -1,6 +1,50 @@
 package cairo
 
-import "log"
+import (
+	"image/png"
+	"log"
+	"os"
+)
+
+//A simple hello world that writes a blue "Hello, world" to hello.png.
+//
+//Adapted from http://cairographics.org/FAQ/#minimal_C_program .
+func Example_helloWorld() {
+	surface, err := NewImageSurface(FormatARGB32, 240, 80)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer surface.Close()
+
+	cr, err := New(surface)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer cr.Close()
+
+	cr.
+		SelectFont("serif", 0, WeightBold).
+		SetFontSize(32).
+		SetSourceColor(Blue).
+		MoveTo(Pt(10, 50)).
+		ShowText("Hello, world")
+
+	img, err := surface.ToImage()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	f, err := os.Create("hello.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	err = png.Encode(f, img)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 
 func ExampleContext_Circle_drawEllipse() {
 	is, err := NewImageSurface(FormatARGB32, 500, 500)
